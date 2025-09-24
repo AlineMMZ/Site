fb957fa19b38357d02709a08eb1bf36567913691
AlineMMZ <alinezardi@gmail.com>
2025-09-24 13:06:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+    <!-- Schema Markup focado no público brasileiro -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": ["ProfessionalService", "HealthAndBeautyBusiness", "MedicalBusiness"],
+        "name": "Aline Medeiros - Psicoterapia Online",
+        "alternateName": ["Aline Medeiros Psicoterapeuta", "Psicoterapia para Brasileiras no Exterior"],
+        "description": "Atendimento psicológico especializado para mulheres brasileiras no exterior. Psicoterapia online, mentoria emocional e consultoria para imigração.",
+        "url": "https://www.psialinemedeiros.com.br",
+        "sameAs": [
+            "https://wa.me/5512996262275"
+        ],
+        "serviceType": [
+            "Psicoterapia Individual Online",
+            "Mentoria Raízes e Recomeços", 
+            "Consultoria Emoção em Movimento",
+            "Apoio Psicológico para Imigrantes",
+            "Terapia em Português"
+        ],
+        "areaServed": {
+            "@type": "Place",
+            "name": "Atendimento Online Global para Brasileiras"
+        },
+        "provider": {
+            "@type": "Person",
+            "name": "Aline Medeiros",
+            "jobTitle": ["Consultora", "Psicoterapeuta", "Mentoria Emocional"],
+            "knowsLanguage": ["pt-BR", "pt"],
+            "nationality": "Brazilian"
+        },
+        "offers": [
+            {
+                "@type": "Offer",
+                "name": "Psicoterapia Individual Online",
+                "serviceType": "Psychotherapy",
+                "availableChannel": {
+                    "@type": "ServiceChannel",
+                    "serviceType": "Online",
+                    "availableLanguage": ["pt-BR", "pt"]
+                }
+            },
+            {
+                "@type": "Offer", 
+                "name": "Mentoria para Brasileiras no Exterior",
+                "serviceType": "Mentoring",
+                "availableChannel": {
+                    "@type": "ServiceChannel",
+                    "serviceType": "Online",
+                    "availableLanguage": ["pt-BR", "pt"]
+                }
+            }
+        ]
+    }
+    </script>
